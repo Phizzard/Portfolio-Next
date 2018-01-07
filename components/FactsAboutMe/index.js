3ddf6258496c539b46d33453e5d82f73da7cbfd0
phizzard <phizzard@gmail.com>
2018-01-07 14:10:10 -0500

    add images for tech and use CardColumns

--- components/FactsAboutMe/index.js
+++ components/FactsAboutMe/index.js
@@ -6,14 +6,14 @@ export default (props) => (
     <div className="facts-about-me page-block">
         <SectionTitle align="center" subTitle="Just a few things about me">Facts About Me</SectionTitle>
         <Row>
-            <Col md="6">
+            <Col md="6" sm="12">
                 <List items={{
                     status: props.items.status,
                     educationLevel: props.items['education-level'],
                     specialty: props.items.specialty
                 }} />
             </Col>
-            <Col md="6">
+            <Col md="6" sm="12">
                 <List items={{
                     languages: props.items.languages,
                     position: props.items.position,
